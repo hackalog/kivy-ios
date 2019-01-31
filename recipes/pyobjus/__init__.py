a6e8190f4621531ca0b0be025d3da11939f3d087
Kjell Wooding <kjell@wooding.org>
2019-01-30 21:27:52 -0500

    use personal fork

--- recipes/pyobjus/__init__.py
+++ recipes/pyobjus/__init__.py
@@ -2,7 +2,8 @@ from toolchain import CythonRecipe
 
 class PyobjusRecipe(CythonRecipe):
     version = "master"
-    url = "https://github.com/kivy/pyobjus/archive/{version}.zip"
+    owner = "hackalog"
+    url = "https://github.com/{owner}/pyobjus/archive/{version}.zip"
     library = "libpyobjus.a"
     depends = ["python"]
     pre_build_ext = True
